9a9081a7e022afdc83c51e0712f118ba62db0a99
prathameshdarokar20 <prathameshdarokar20@gmail.com>
2026-08-18 22:17:42 +0530

    Create Employee.sql

---
+++ Employee.sql
@@ -0,0 +1,8 @@
+use prathamesh;
+create table employee(
+employee_id int primary key,
+first_name varchar(50),
+last_name varchar(50)
+);
+insert into employee value (101,'Avinash','Shende'),(102,'Abhi','Rao');
+select*from employee;
